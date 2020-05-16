f935f7d758995841611689a1bdd2102cc635468b
Karthik Nishanth <Karthik.nishanth06@gmail.com>
2020-05-16 14:38:00 +0530

    Start

--- pages/index.js
+++ pages/index.js
@@ -7,8 +7,7 @@ export default function Home() {
   return (
     <div className="app">
       <Nav />
-      <Banner />
-      {""}
+      <Banner />{" "}
       <Row
         title="NETFLIX ORIGINALS"
         isLargeRow
